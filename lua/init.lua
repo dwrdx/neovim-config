e54ccf1b32059f22d53dbb1fa83013e7ffd2b23b
Edward XU <edward.xu@hotmail.com>
2021-12-12 09:13:33 +0800

    require mywords.nvim plugin in init.lua

--- lua/init.lua
+++ lua/init.lua
@@ -10,6 +10,7 @@ local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protoco
 local cmp          = require('cmp')
 local toggleterm   = require('toggleterm')
 local tree_sitter  = require('nvim-treesitter.configs')
+local mywords      = require('mywords')
 
 
 g.symbols_outline = {
